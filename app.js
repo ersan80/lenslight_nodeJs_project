52782520f141a25dde04b0fe11b97689beed8253
ersan80 <ersan.kuleli80@gmail.com>
2022-12-29 22:06:06 +0300

    aaa

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("indeks 2");
-});
+// ? ejs template
 
+app.set("view engine","ejs")
+
+// ? static files middleware
+
+app.use(express.static("public"));
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 app.listen(port, () => {
   console.log(`Aplication port ${port}`);
 });
